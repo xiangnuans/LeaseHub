9acf440a7d067e0e9014735f33dc5145127bb3c2
Guo Hong <1352118502@qq.com>
2024-10-06 01:22:02 +0800

    feat: 屏幕适配

--- apps/web/components/stake/travel.tsx
+++ apps/web/components/stake/travel.tsx
@@ -1,27 +1,36 @@
-'use client';
-import React from 'react';
-import { Img, Button, Heading, Text, Input } from '@/components/';
-import BoxImage from '@/assets/images/stake/travel-box.svg';
-import SolIcon from '@/assets/icons/travel-sol.svg';
-import BackIcon from '@/assets/icons/travel-back-arrow.svg';
-import CopyIcon from '@/assets/icons/copy.svg';
-import RightArrowIcon from '@/assets/icons/travel-right-arrow.svg';
+"use client";
+import React from "react";
+import { Img, Button, Heading, Text, Input } from "@/components/";
+import BoxImage from "@/assets/images/stake/travel-box.svg";
+import SolIcon from "@/assets/icons/travel-sol.svg";
+import BackIcon from "@/assets/icons/travel-back-arrow.svg";
+import CopyIcon from "@/assets/icons/copy.svg";
+import RightArrowIcon from "@/assets/icons/travel-right-arrow.svg";
+import { useRouter } from "next/navigation";
 
 export default function StakeTravel() {
+  const router = useRouter();
+
+  const handleBack = () => {
+    router.back();
+  };
   return (
-    <div className="relative mb-1 h-[800px] content-center self-stretch lg:h-auto md:h-auto">
-      <div className="container-xs flex justify-center lg:px-5 md:px-5">
+    <div className="relative w-full content-center self-stretch lg:h-auto md:h-auto">
+      <div className="flex justify-center lg:px-5 md:px-5">
         <Img
           src={BoxImage}
           alt=""
           width={1448}
           height={800}
-          className="h-[800px] w-full lg:h-auto md:h-auto"
+          className="h-auto w-full lg:h-auto md:h-auto"
         />
       </div>
-      <div className="container-xs absolute left-0 right-0 top-[5%] my-auto flex justify-center px-10 lg:px-5 md:px-5">
+      <div className="absolute left-0 right-0 top-[10%] flex px-12 lg:px-5 md:px-5">
         <div className="w-full">
-          <div className="flex items-center gap-8">
+          <div
+            className="flex items-center gap-8 cursor-pointer"
+            onClick={handleBack}
+          >
             <Button
               color="black_900_02"
               size="xs"
@@ -37,58 +46,60 @@ export default function StakeTravel() {
               Traveler
             </Heading>
           </div>
-          <div className="mx-[66px] mt-[30px] md:mx-0">
+          <div className="flex items-center gap-2.5">
+            <Text
+              size="texts"
+              as="p"
+              className="text-[14px] font-normal text-gray-400"
+            >
+              {"0xBBB6A745sadasd2dsds6hn9"}
+            </Text>
+            <Img
+              src={CopyIcon}
+              width={18}
+              height={18}
+              alt="Image"
+              className="h-[18px] w-[18px]"
+            />
+          </div>
+        </div>
+      </div>
+      <div className="absolute left-0 right-0 top-[12%] flex px-12 lg:px-5 md:px-5">
+        <div className="w-full">
+          <div className="mt-[30px] md:mx-0">
             <div className="flex flex-col gap-7">
-              <div className="flex items-start justify-center sm:flex-col">
-                <div className="flex items-center gap-2.5">
-                  <Text
-                    size="texts"
-                    as="p"
-                    className="text-[14px] font-normal text-gray-400"
-                  >
-                    {'0xBBB6A745sadasd2dsds6hn9'}
-                  </Text>
-                  <Img
-                    src={CopyIcon}
-                    width={18}
-                    height={18}
-                    alt="Image"
-                    className="h-[18px] w-[18px]"
-                  />
-                </div>
-                <div className="mt-2 flex flex-1 flex-col  items-start slef-end pl-[120px] pr-14 lg:pl-8 md:px-5 sm:self-stretch sm:px-4">
-                  <Img
-                    src={SolIcon}
-                    width={48}
-                    height={48}
-                    alt="Image"
-                    className="h-[48px] w-[48px] md:ml-0"
-                  />
-                  <Text
-                    size="texts"
-                    as="p"
-                    className="ml-[38px] mt-6 text-[14px] font-normal text-white-a700 md:ml-0"
-                  >
-                    Current Stake
-                  </Text>
-                  <Heading
-                    size="heading4xl"
-                    as="h2"
-                    className="font-dinalternate text-[64px] font-bold !text-lime-400 lg:text-[48px] md:text-[48px]"
-                  >
-                    456.02
-                  </Heading>
-                  <Text
-                    size="textxl"
-                    as="p"
-                    className="ml-[70px] text-[20px] font-normal text-white-a700 lg:text-[17px] md:mr-0"
-                  >
-                    SOL
-                  </Text>
-                </div>
+              <div className="mt-1 flex flex-col items-center slef-end md:px-5 sm:self-stretch sm:px-4">
+                <Img
+                  src={SolIcon}
+                  width={48}
+                  height={48}
+                  alt="Image"
+                  className="h-[48px] w-[48px]"
+                />
+                <Text
+                  size="texts"
+                  as="p"
+                  className="mt-6 text-[14px] font-normal text-white-a700"
+                >
+                  Current Stake
+                </Text>
+                <Heading
+                  size="heading2xl"
+                  as="h2"
+                  className="font-dinalternate text-[64px] font-bold !text-lime-400 lg:text-[48px] md:text-[32px]"
+                >
+                  456.02
+                </Heading>
+                <Text
+                  size="textxl"
+                  as="p"
+                  className="text-[20px] font-normal text-white-a700 lg:text-[17px] md:mr-0"
+                >
+                  SOL
+                </Text>
               </div>
-              <div className="rowcurrentstake_border ml-[174px] mr-[170px] flex items-end justify-center rounded-[40px] bg-gradient2 p-6 md:mx-0 md:flex-col sm:p-4">
-                <div className="mt-3 flex flex-col items-start gap-2 px-[18px]">
+              <div className="rowcurrentstake_border mx-[12%] flex items-center justify-center rounded-[40px] bg-gradient2 p-6 md:mx-0 md:flex-col sm:p-4">
+                <div className="flex flex-col items-start gap-2 px-[18px]">
                   <Text
                     size="textlg"
                     as="p"
@@ -98,7 +109,7 @@ export default function StakeTravel() {
                   </Text>
                   <Heading
                     size="heading3xl"
-                    as={'h3'}
+                    as={"h3"}
                     className="font-dinalternate text-[36px] font-bold !text-lime-400 lg:text-[30px] md:text-[30px] sm:text-[28px]"
                   >
                     8,560.00
@@ -122,7 +133,7 @@ export default function StakeTravel() {
                   </Text>
                   <Heading
                     size="heading3xl"
-                    as={'h4'}
+                    as={"h4"}
                     className="mr-1.5 font-dinalternate text-[36px] font-bold !text-lime-400 lg:text-[30px] md:mr-0 md:text-[30px] sm:text-[28px]"
                   >
                     230,00
@@ -144,15 +155,15 @@ export default function StakeTravel() {
                       width={46}
                       height={6}
                       alt=""
-                      className="mb-1.5 mt-2.5 h-[6px] w-[46px]"
+                      className="h-[6px] w-[46px]"
                     />
                   }
-                  className="mb-[22px] min-w-[228px] gap-[26px] rounded-[10px] border border-solid  !border-lime-400 px-[33px] font-semibold !text-black-900_02 md:px-5 bg-lime-400"
+                  className="min-w-[228px] gap-[26px] rounded-[10px] border border-solid  !border-lime-400 px-[33px] font-semibold !text-black-900_02 md:px-5 bg-lime-400"
                 >
                   Harvest
                 </Button>
               </div>
-              <div className="ml-14 mt-[30px] flex self-stretch md:ml-0 md:flex-col">
+              <div className="mt-[24px] flex self-stretch md:ml-0 md:flex-col">
                 <div className="flex flex-1 flex-col gap-2.5 md:self-stretch">
                   <div className="flex p-2.5">
                     <Text
@@ -167,7 +178,7 @@ export default function StakeTravel() {
                       <Input
                         shape="round"
                         name="inputnumber_one"
-                        placeholder={'50'}
+                        placeholder={"50"}
                         className="mx-auto flex-grow rounded-[10px] px-4 font-semibold"
                       />
                       <Button
@@ -182,7 +193,7 @@ export default function StakeTravel() {
                     <Button
                       size="xl"
                       shape="round"
-                      className="min-w-[178px] rounded-[10px] px-[34px] font-bold sm:px-4 bg-lime-400"
+                      className="min-w-[178px] rounded-[10px] px-[34px] font-bold sm:px-4 bg-lime-400 text-black-900"
                     >
                       Augment
                     </Button>
@@ -202,7 +213,7 @@ export default function StakeTravel() {
                       <Input
                         shape="round"
                         name="inputnumber"
-                        placeholder={'50'}
+                        placeholder={"50"}
                         className="mx-auto rounded-[10px] px-4"
                       />
                       <Button
@@ -217,7 +228,7 @@ export default function StakeTravel() {
                     <Button
                       size="xl"
                       shape="round"
-                      className="min-w-[178px] rounded-[10px] px-[34px] font-bold sm:px-4 bg-lime-400"
+                      className="min-w-[178px] rounded-[10px] px-[34px] font-bold sm:px-4 bg-lime-400 text-black-900"
                     >
                       Whithdraw
                     </Button>
