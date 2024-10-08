768d8d19e7ab7d7b13869099e6e263925b5a5b83
Guo Hong <1352118502@qq.com>
2024-10-08 23:05:15 +0800

    chore: deply

--- apps/web/next.config.js
+++ apps/web/next.config.js
@@ -1,6 +1,8 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
+  output: 'export',
   basePath: '/LeaseHub',
+  assetPrefix: '/LeaseHub',
   eslint: {
     ignoreDuringBuilds: true,
   },
